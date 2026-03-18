8c16dd56411f1715be464952cf70165bba0c8fee
mhackieee <54742332+mhackieee@users.noreply.github.com>
2026-03-18 11:00:29 +0800

    Add route to serve sw.js at root

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
+from flask import send_from_directory, current_app
+
+@app.route('/sw.js')
+def service_worker():
+    return send_from_directory(current_app.static_folder, 'sw.js', mimetype='application/javascript')
+
 engine = create_engine('sqlite:///../data/logs.sqlite')
 
 @app.route('/')
